75c9e9eeed55a1b878350a00e533cf05cb549529
vimleshtech <35063663+vimleshtech@users.noreply.github.com>
2019-02-01 11:23:10 +0530

    Test

--- RegressionExample.py
+++ RegressionExample.py
@@ -18,16 +18,17 @@ def estimate_coef(x, y):
   
     return(b_0, b_1) 
   
-def plot_regression_line(x, y, b): 
-    # plotting the actual points as scatter plot 
+def plot_regression_line(x, y, b):
+# plotting the actual points as scatter plot 
     plt.scatter(x, y, color = "m", 
                marker = "o") 
-  
+    
     # predicted response vector 
-    y_pred = b[0] + b[1]*x 
+    
   
     # plotting the regression line 
-    plt.plot(x, y_pred, color = "g") 
+    plt.plot(x_newData, y_pred, color = "g")
+    plt.plot(x,y,'.')
   
     # putting labels 
     plt.xlabel('x-label') 
@@ -39,13 +40,15 @@ def plot_regression_line(x, y, b):
 
 # observations 
 x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) 
-y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12]) 
-  
+y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+x_newData=np.array([10,11,12,17])
+ 
 # estimating coefficients 
 b = estimate_coef(x, y) 
 print("Estimated coefficients:\nb_0 = {} \n b_1 = {}".format(b[0], b[1])) 
-
-# plotting regression line 
-plot_regression_line(x, y, b) 
+y_pred = b[0] + b[1]*x_newData 
+# plotting regression line
+plot_regression_line(x, y, b)
+plot_regression_line(x_newData, y_pred, b) 
   
 
